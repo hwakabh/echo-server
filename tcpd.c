f258e20ef9c6e8bce5d4571e2ef2389ed7822eef
hwakabh <hrykwkbys1024@gmail.com>
2019-10-16 22:10:08 +0900

    #2 Added logics to check number of command line arguments, and made them show help menu if wrong.

--- tcpd.c
+++ tcpd.c
@@ -5,6 +5,13 @@ void show_help_menu(void);
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        printf("Missing arguments, please specify port number.\n");
+        show_help_menu();
+        return 128;
+    }
+
+
     if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
         show_help_menu();
         return 0;
