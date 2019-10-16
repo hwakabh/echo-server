e40ccfa2ebaf888dc116cd664e581391409c1b90
hwakabh <hrykwkbys1024@gmail.com>
2019-10-16 13:39:04 +0900

    #3 Added conditions to check argv[1] strings for showing help menu.

--- tcpd.c
+++ tcpd.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+        printf("WIP: help menu here ...\n");
+        return 0;
+    }
+
     printf("Serving HTTP on 0.0.0.0 port %s (http://0.0.0.0:%s/) ...\n", argv[1], argv[1]);
     printf("Quit the server with CONTROL-C.\n");
+
     return 0;
 }
