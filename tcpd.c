515caba053e399e27eaa05cecbadbbe10816853c
hwakabh <hrykwkbys1024@gmail.com>
2019-10-21 18:59:32 +0900

    #4 Added basic functions to open socket.

--- tcpd.c
+++ tcpd.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 void show_help_menu(void);
 
@@ -23,6 +27,26 @@ int main(int argc, char *argv[])
 
     printf("Serving HTTP on 0.0.0.0 port %s (http://0.0.0.0:%s/) ...\n", argv[1], argv[1]);
     printf("Quit the server with CONTROL-C.\n");
+
+    // Create sockets
+    int rsock = socket(AF_INET, SOCK_STREAM, 0);
+    // Assign address to socket
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8088);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    // bind socket
+    int ret = bind(rsock, (struct sockaddr *)&addr, sizeof(addr));
+    // listen
+    listen(rsock, 5);
+    // awaiting connection
+    int wsock;
+    int len;
+    struct sockaddr_in client;
+
+    len = sizeof(client);
+    wsock = accept(rsock, (struct sockaddr *)&client, &len);
+    write(wsock, "HTTP1.1 200 OK\n", 15);
 
     return 0;
 }
